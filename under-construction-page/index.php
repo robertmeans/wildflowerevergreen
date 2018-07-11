09be4fac23035253f11e8f699e610b3dbbf50143
Evergreen Web Design <info@evergreenwebdesign.com>
2018-07-11 11:00:51 -0600

    squarespace custom files

--- under-construction-page/index.php
+++ under-construction-page/index.php
@@ -20,7 +20,7 @@
 	<link rel="icon" type="image/ico" href="_images/favicon.ico">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 	<link href='https://fonts.googleapis.com/css?family=Courgette|Lato' rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" href="aos.css" />
@@ -59,6 +59,7 @@
 	<div class="uc">
 		<img src="_images/under-construction.png">
 		<div class="uc-dark">
+			<a class="scroll-down" href="#menu"><i class="fas fa-angle-down"></i>&nbsp;&nbsp;&nbsp; Scroll Down &nbsp;&nbsp;&nbsp;<i class="fas fa-angle-down"></i></a>
 			<h2>Wildflower Cafe</h2>
 			<p>Monday - Saturday 7:30 AM - 2:00 PM</p>
 			<p>Sunday 8:00 AM - 2:00 PM</p>
